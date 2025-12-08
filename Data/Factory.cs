b864e2a7f11c7847b9e3297f4e6c496bf12d5a25
Robert Bill <rob@slgm.ch>
2025-12-08 11:43:33 +0100

    Copy source code

---
+++ Data/Factory.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Calendare.Data;
+
+public class TestContextFactory : IDesignTimeDbContextFactory<CalendareContext>
+{
+    public TestContextFactory() { }
+
+    public CalendareContext CreateDbContext(string[] args)
+    {
+
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddUserSecrets<TestContextFactory>()
+            .Build();
+        var optionsBuilder = new DbContextOptionsBuilder<CalendareContext>();
+        optionsBuilder.ConfigureCalendareNpgsql(configuration.GetSection("Postgresql"));
+        return new CalendareContext(optionsBuilder.Options);
+    }
+}
